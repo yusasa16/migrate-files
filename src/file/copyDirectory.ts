bddf8f486b92e636d8ce28e94216bbb94cf9d99e
yusasa16 <yusasa16@gmail.com>
2023-02-07 22:25:31 +0900

    copyDirectory

--- src/file/copyDirectory.ts
+++ src/file/copyDirectory.ts
@@ -1,13 +1,31 @@
 import fs from 'fs';
 import path from 'path';
 
+/**
+ * ディレクトリを複製する
+ * @param src 複製元のディレクトリパス
+ * @param dist コピーを吐き出す先のディレクトリパス
+ */
 export function copyDirectory(src: string, dist: string) {
+	// コピー先のディレクトリが存在しなかった場合、ディレクトリを作成する。
+	if(!fs.existsSync(dist)) {
+		fs.mkdir(dist, { recursive: true }, (err) => {
+			if(err) {
+				console.error(err);
+				return;
+			}
+			console.log('dist directory has been created.');
+		})
+	}
+
+	// コピー元の第一階層を取得
 	fs.readdir(src, (err, items) => {
 		if(err) {
 			console.error(err);
 			return;
 		}
 
+		// 第一階層のアイテムごとにループ
 		for(const item of items) {
 			const srcPath = path.join(src, item);
 			const distPath = path.join(dist, item);
@@ -18,6 +36,8 @@ export function copyDirectory(src: string, dist: string) {
 					return;
 				}
 
+				// ファイルであればコピー、
+				// ディレクトリであればコピー先にディレクトリを作成して再度関数実行
 				if(stats.isFile()) {
 					fs.copyFile(srcPath, distPath, (err) => {
 						if(err) {
